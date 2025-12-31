2c9f51600bac08347b069e9fea9ceef9a996b818
Winnie <winnie.wt.chen@gmail.com>
2025-12-30 22:18:16 -0800

    feat: Add path aliasing in Vite configuration to simplify module imports and improve code organization.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     react(),
     VitePWA({
